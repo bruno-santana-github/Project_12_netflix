85318504da5ed08b455b1e7de0af056ab399c614
Bruno Santana <brunomoraes.bm.bm@gmail.com>
2023-03-29 22:15:08 -0300

    Project12 - Netflix-copy

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+        <link href="css/template.css" rel="stylesheet"/>
+        <title>Project 12 | Netflix Copy</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="bruno s. moraes, bruno, moraes, bruno santana, descomplica university, descomplica, university, student, front-end, back-end, fullstack,  web, website, computer, software, developer, engineer, designer, co-op" />
+        <meta name="author" content="Bruno Moraes" />
+        <meta name="description" content="Fullstack Developer Jr,  Enthusiast, and everything in between." />
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta name="author" content="Bruno Santana"/>
+        <!----------Tags SEO Facebook--------->
+        <meta property="og:url"                content="" />
+        <meta property="og:type"               content="" />
+        <meta property="og:title"              content="" />
+        <meta prHow much does culture influence creative thinking?" />
+        <meta property="og:image"              content="" />
+        <!----------Tags SEO Twitter--------->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="A Twitter for My Sister" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <!----------Links de Style--------->
+        <link href="images/logo.png" type="image-x/png" rel="shortcut icon"/>
+    </head>
+    <body>
+        <header>
+            <div class="center">
+                <div class="header-left">
+                    <a href=""><img src="images/logo_netflix.png" /></a>
+                    <div class="menu-header">
+                        <ul>
+                            <li><a href="#">Start</a></li>
+                            <li><a href="#">Series</a></li>
+                            <li><a href="#">Films</a></li>
+                            <li><a href="#">Recently Added</a></li>
+                            <li><a href="#">My list</a></li>
+                        </ul>
+                    </div><!---menu-header-->
+                </div><!---header-left-->
+                <div class="header-right">
+                    <div class="search">
+                        <i class="icon-search"></i>
+                    </div><!--search--->
+                    <a href="">INFANTIL</a>
+                    <i class="icon-gift"></i>
+                    <div class="notifications">
+                        <div class="circle-notifications">
+                            <span>10</span>
+                        </div><!----circle-notifications-->
+                        <i class="icon-bell-alt"></i>
+                    </div><!---notifications--->
+                </div><!---header-right-->
+                <div class="clear"></div>
+            </div><!---center-->
+        </header>
+
+        <section class="destaque">
+            <div class="overlay"></div><!--overlay-->
+            <video id="video-destaque" autoplay loop>
+                <source src="images/arrow.mp4" type="video/mp4">
+	            Your browser does not support HTML5 video.
+            </video><!---video-destaque-->
+            <div class="texto-destaque">
+                <h2>Lorem Ipsum</h2>
+                <h3>Watch full season 9 now!</h3>
+			    <p>Here's the full description of my series or movie.</p>
+			    <br />
+                <a href=""><i class="icon-play"></i> Watch Here</a>
+			    <a href=""><i class="icon-play"></i> Watch Here</a>
+			    <a href=""><i class="icon-play"></i>Watch Here</a>
+            </div><!--texto-destaque-->
+        </section><!---destque--->
+
+        <section class="vitrine">
+            <div class="vitrine-flex">
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image1.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+                
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image2.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image3.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image4.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image5.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image6.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image7.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image8.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image9.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image10.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image11.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image12.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+            </div><!---vitrine-flex-->
+        </section><!---vitrine--->
+
+        <section class="vitrine">
+            <div class="vitrine-flex">
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image10.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+                
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image9.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image8.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image7.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image6.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image5.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image4.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image3.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image2.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image2.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image1.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image12.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+            </div><!---vitrine-flex-->
+        </section><!---vitrine--->
+
+        <section class="vitrine">
+            <div class="vitrine-flex">
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image1.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+                
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image2.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image3.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image4.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image5.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image6.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image7.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image8.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image9.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image10.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image11.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+
+                <div class="vitrine-single-banner">
+                    <div class="vitrine-wraper" style="background-image:url('images/image12.jpg');"></div>
+                </div><!---vitrine-single-banner-->
+            </div><!---vitrine-flex-->
+        </section><!---vitrine--->
+
+        
+
+        <script src="js/jquery.js"></script>
+	    <script src="js/slick.min.js"></script>
+        <script>
+            $('.vitrine-flex').slick({
+                    dots: false,
+                    arrows:false,
+                infinite: false,
+                centerMode: false,
+                speed:1000,
+                slidesToShow: 6,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    pauseOnHover:false,
+                        responsive: [
+                    {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3
+                    }
+                }]
+                    });
+
+                $('.vitrine-wraper').hover(function(){
+                    $(this).css('z-index','1000');
+                })
+
+                $('.vitrine-wraper').mouseout(function(){
+                    $(this).css('z-index','0');
+            })
+	</script>
+    </body>
+</html>
